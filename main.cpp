53a0a5259a05cab1a582e3feb7c8abd1b377ab6f
iluv0rangejuice <54556887+iluv0rangejuice@users.noreply.github.com>
2019-10-31 12:14:57 -0700

    Updated main.cpp
    Updated code to work for milestone 2

--- main.cpp
+++ main.cpp
@@ -12,37 +12,39 @@
 #include "PpmDocument.hpp"
 using namespace std;
 
+
+
 int main(int argc, char* argv[])
 { 
-    string item;
-    
+    int item (127);
     //catching a_file
     vector <string> data = readFile("bunny.ppm");
-    
     for (int i = 3; i<data.size(); i++)
     {
-
         //reading into a_file
         istringstream line {data [i]};
         while (line.good() == true)
         {
-            //changing bunny.ppm into a int
-            string size_type;
-            int i_bunny = stoi ("bunny.ppm");
+            //changing bunny.ppm into an int
+            int int_bunny = stoi ("bunny.ppm");
             
-            //setting new index [j] &
-            //increasing index by intervals of 3
-            for(int j = 0; j<data.size(); j+=3)
+            //setting new index [j] & changing int
+            for(int j = 0; j<data.size(); j++)
             {
-                if (data.size() <= item);
+                //if x > 255 -> 255 || if x < 255 -> 0
+                if (data.size() > 127)
+                {
+                    item = 255;
+                }
+                else if (data.size() < 127)
                 {
                     item = 0;
                 }
-            
+                                
             }
+            string to_string("bunny.ppm");
         }
+    cout << "bunny.ppm" << endl;
     }
-    cout << data.size();
-
 return 0;
 }
